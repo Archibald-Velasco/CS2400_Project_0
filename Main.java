49358428ab707f40e8881e32d4259589964012da
Archibald <archibaldvelasco2005@gmail.com>
2026-02-01 23:45:30 -0800

    fixed bug when calculating product

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
         System.out.println("The sum of " + a + " and " + b + " is: " + sum);
 
         // Intentional bug for debugging practice
-        int product = a - b; // This should be a * b
+        int product = a * b;
         System.out.println("The product of " + a + " and " + b + " is: " + product);
     }
 }
