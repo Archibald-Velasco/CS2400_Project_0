7c7a1dcee5c21027919d86ea8ce1957dab08da52
Archibald <archibaldvelasco2005@gmail.com>
2026-02-01 23:18:00 -0800

    Add .gitignore file to exclude unnecessary files

--- Main.java
+++ Main.java
@@ -3,7 +3,6 @@ public class Main {
         int a = 5;
         int b = 10;
         int sum = a + b;
-
         System.out.println("The sum of " + a + " and " + b + " is: " + sum);
 
         // Intentional bug for debugging practice
