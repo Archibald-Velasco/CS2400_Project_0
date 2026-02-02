841f7f322e43b6f3b53cf52fb06e06e1770d4070
Archibald <archibaldvelasco2005@gmail.com>
2026-02-01 23:04:24 -0800

    First

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+
+        // Intentional bug for debugging practice
+        int product = a - b; // This should be a * b
+        System.out.println("The product of " + a + " and " + b + " is: " + product);
+    }
+}
